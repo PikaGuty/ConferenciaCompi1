64da8bf8dadc2eae1d8478b4da4957a9ec734443
Javier Gutierrez <3024021520101@ingenieria.usac.edu.gt>
2024-09-09 12:21:08 -0600

    corregido

---
+++ src/Routes/cliente_routes.js
@@ -0,0 +1,12 @@
+const express = require ('express')
+const router = express.Router()
+const control = require("../Controllers/cliente_controller")
+
+router.get("/cliente", control.index)
+
+router.get("/tsim", control.TSimbolos)
+router.post("/analizar", control.Analizar)
+router.get("/ast", control.Ast)
+router.get("/err", control.TErrores)
+
+module.exports = router
